756275e0b30122919b5027ff7b546c62c657b11f
amolpai99 <amolpai070499@gmail.com>
2022-10-27 20:54:06 +0530

    Coursera Front-End Angular course - Week 3: Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -65,6 +66,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     MatProgressSpinnerModule,
     ReactiveFormsModule
   ],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -20,16 +22,51 @@ export class DishdetailComponent implements OnInit {
 
   faLeft = faChevronLeft;
   faRight = faChevronRight;
+  @ViewChild('cform') commentFormDirective: NgForm;
+
+  commentForm: FormGroup
+  comment: Comment
+
+  formErrors: {[key: string]: string} = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages: {[key: string]: any} = {
+    'author': {
+      'required': 'Author name is required',
+      'minlength': 'Name must be atleast 2 characters',
+    },
+    'comment': {
+      'required': 'Comment is required',
+    }
+  };
 
   constructor(private dishService: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+      this.createForm();
+    }
 
   ngOnInit(): void {
     this.dishService.getDishIds().subscribe((ids) => this.dishIds = ids);
     this.route.params
       .pipe(switchMap((params) => this.dishService.getDish(params['id'])))
       .subscribe((dish) => {this.dish = dish; this.setPrevNext(dish.id)});
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      rating: 5,
+      comment: ['', Validators.required]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
 
   setPrevNext(dishId: string) {
@@ -42,4 +79,42 @@ export class DishdetailComponent implements OnInit {
     this.location.back();
   }
 
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if(control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+    this.commentFormDirective.resetForm();
+
+    const date = new Date().toISOString();
+    this.comment.date = date;
+    
+    this.dish.comments.push(this.comment);
+  }
 }
